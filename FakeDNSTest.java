20b649bcb3d6f3bbb99b8a8fbed1e19dfaf6f8a1
YuChunlei <147286318@qq.com>
2018-06-15 13:43:16 +0800

    Create FakeDNSTest.java

---
+++ FakeDNSTest.java
@@ -0,0 +1,20 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+//run with option  --add-opens java.base/java.net=ALL-UNNAMED
+
+public class FakeDNSTest {
+
+    public static void main(String[] args){     
+        FakeDNS fakeDns=FakeDNS.getInstance();
+        fakeDns.install();
+        System.setProperty("illegal-access", "deny");
+        try {
+            fakeDns.addForwardResolution("test",InetAddress.getByName("192.168.142.2"));
+            System.out.println(InetAddress.getByName("www.163.com").getHostAddress());
+            System.out.println(InetAddress.getByName("test").getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
