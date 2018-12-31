4e580c70581fca43808f3193f6ed73e74c78dbad
YuChunlei <147286318@qq.com>
2018-12-31 21:46:25 +0800

    Update sysbench_mysql.sh

--- sysbench_mysql.sh
+++ sysbench_mysql.sh
@@ -1,5 +1,12 @@
 sysbench --test=oltp --oltp-table-size=10000 --db-driver=mysql --mysql-db=test --mysql-user=root --mysql-host=127.0.0.1 --mysql-password=### prepare
 sysbench --test=oltp --oltp-table-size=10000 --db-driver=mysql --mysql-db=test --mysql-user=root --mysql-password=### --max-time=60 --oltp-read-only=on --max-requests=0 --num-threads=8 run
 sysbench --test=oltp --db-driver=mysql --mysql-db=test --mysql-user=root --mysql-password=### cleanup
-
+./bin/sysbench --test=./share/tests/db/oltp.lua \
+--mysql-host=10.0.201.36 --mysql-port=8066 --mysql-user=ecuser --mysql-password=ecuser \
+--mysql-db=dbtest1a --oltp-tables-count=10 --oltp-table-size=500000 \
+--report-interval=10 --oltp-dist-type=uniform --rand-init=on --max-requests=0 \
+--oltp-test-mode=nontrx --oltp-nontrx-mode=select \
+--oltp-read-only=on --oltp-skip-trx=on \
+--max-time=120 --num-threads=12 \
+[prepare|run|cleanup]
 sysbench /usr/share/sysbench/tests/include/oltp_legacy/insert.lua --oltp-table-size=100000 --db-driver=mysql --mysql-db=test --mysql-user=root --mysql-host=127.0.0.1 --mysql-password=###  --threads=2 run
